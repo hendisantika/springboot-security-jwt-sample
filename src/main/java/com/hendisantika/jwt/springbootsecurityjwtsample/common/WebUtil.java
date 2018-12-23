b64f1084962ae4043f95be8641d3299cce98aebc
Hendi Santika <hendisantika@gmail.com>
2018-12-24 06:47:11 +0700

    #8 - Add Web Util common

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/common/WebUtil.java
@@ -0,0 +1,35 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.common;
+
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-24
+ * Time: 06:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebUtil {
+    private static final String XML_HTTP_REQUEST = "XMLHttpRequest";
+    private static final String X_REQUESTED_WITH = "X-Requested-With";
+
+    private static final String CONTENT_TYPE = "Content-type";
+    private static final String CONTENT_TYPE_JSON = "application/json";
+
+    public static boolean isAjax(HttpServletRequest request) {
+        return XML_HTTP_REQUEST.equals(request.getHeader(X_REQUESTED_WITH));
+    }
+
+    public static boolean isAjax(SavedRequest request) {
+        return request.getHeaderValues(X_REQUESTED_WITH).contains(XML_HTTP_REQUEST);
+    }
+
+    public static boolean isContentTypeJson(SavedRequest request) {
+        return request.getHeaderValues(CONTENT_TYPE).contains(CONTENT_TYPE_JSON);
+    }
+}
