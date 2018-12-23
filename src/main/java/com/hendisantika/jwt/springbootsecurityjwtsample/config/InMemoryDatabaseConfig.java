5efbfd5ce0926ffc098f8eea9e52b35cd5014e2c
Hendi Santika <hendisantika@gmail.com>
2018-12-24 06:47:56 +0700

    #9 - Add In Memory Database Config

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/config/InMemoryDatabaseConfig.java
@@ -0,0 +1,26 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-24
+ * Time: 06:47
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class InMemoryDatabaseConfig {
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/console/*");
+        return registration;
+    }
+}
