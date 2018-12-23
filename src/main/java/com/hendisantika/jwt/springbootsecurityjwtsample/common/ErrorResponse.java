886ef2cb97ec3fdd5b64f9f9dcfb611e52671566
Hendi Santika <hendisantika@gmail.com>
2018-12-24 06:46:27 +0700

    #7 - Add Error Response common

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/common/ErrorResponse.java
@@ -0,0 +1,55 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.common;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-24
+ * Time: 06:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class ErrorResponse {
+    // HTTP Response Status Code
+    private final HttpStatus status;
+
+    // General Error message
+    private final String message;
+
+    // Error code
+    private final ErrorCode errorCode;
+
+    private final Date timestamp;
+
+    protected ErrorResponse(final String message, final ErrorCode errorCode, HttpStatus status) {
+        this.message = message;
+        this.errorCode = errorCode;
+        this.status = status;
+        this.timestamp = new java.util.Date();
+    }
+
+    public static ErrorResponse of(final String message, final ErrorCode errorCode, HttpStatus status) {
+        return new ErrorResponse(message, errorCode, status);
+    }
+
+    public Integer getStatus() {
+        return status.value();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+}
