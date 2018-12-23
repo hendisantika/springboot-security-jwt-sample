b03a1148b7294ec346561624f70aa22112e59a03
Hendi Santika <hendisantika@gmail.com>
2018-12-24 06:39:20 +0700

    #1 - First commit

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/SpringbootSecurityJwtSampleApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJwtSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityJwtSampleApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/jwt/springbootsecurityjwtsample/SpringbootSecurityJwtSampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSecurityJwtSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
