09c2b6356f9c371a52b911914f85bd5524b397cb
Hendi Santika <hendisantika@gmail.com>
2018-12-25 05:39:42 +0700

    #23 - Add Invalid JWT Token

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/exceptions/InvalidJwtToken.java
@@ -0,0 +1,15 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.exceptions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 05:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvalidJwtToken extends RuntimeException {
+    private static final long serialVersionUID = -294671188037098603L;
+}
