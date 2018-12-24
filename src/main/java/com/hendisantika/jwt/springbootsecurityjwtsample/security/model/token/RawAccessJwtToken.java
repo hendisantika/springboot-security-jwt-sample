89fa3e9f5eee8361f11272721a138b4ee70bebf7
Hendi Santika <hendisantika@gmail.com>
2018-12-25 05:39:06 +0700

    #22 - Add Raw Access JWT Token

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/model/token/RawAccessJwtToken.java
@@ -0,0 +1,50 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.model.token;
+
+import com.hendisantika.jwt.springbootsecurityjwtsample.security.exceptions.JwtExpiredTokenException;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 05:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class RawAccessJwtToken implements JwtToken {
+    private static Logger logger = LoggerFactory.getLogger(RawAccessJwtToken.class);
+
+    private String token;
+
+    public RawAccessJwtToken(String token) {
+        this.token = token;
+    }
+
+    /**
+     * Parses and validates JWT Token signature.
+     *
+     * @throws BadCredentialsException
+     * @throws JwtExpiredTokenException
+     */
+    public Jws<Claims> parseClaims(String signingKey) {
+        try {
+            return Jwts.parser().setSigningKey(signingKey).parseClaimsJws(this.token);
+        } catch (UnsupportedJwtException | MalformedJwtException | IllegalArgumentException | SignatureException ex) {
+            logger.error("Invalid JWT Token", ex);
+            throw new BadCredentialsException("Invalid JWT token: ", ex);
+        } catch (ExpiredJwtException expiredEx) {
+            logger.info("JWT Token is expired", expiredEx);
+            throw new JwtExpiredTokenException(this, "JWT Token expired", expiredEx);
+        }
+    }
+
+    @Override
+    public String getToken() {
+        return token;
+    }
+}
