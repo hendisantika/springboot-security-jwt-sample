139d938db20ce69d176610e97fd49d0e817e92e6
Hendi Santika <hendisantika@gmail.com>
2018-12-25 05:36:37 +0700

    #20 - Add JWT Token Factory

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/model/token/JwtTokenFactory.java
@@ -0,0 +1,94 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.model.token;
+
+import com.hendisantika.jwt.springbootsecurityjwtsample.security.config.JwtSettings;
+import com.hendisantika.jwt.springbootsecurityjwtsample.security.model.Scopes;
+import com.hendisantika.jwt.springbootsecurityjwtsample.security.model.UserContext;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 05:33
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtTokenFactory {
+    private final JwtSettings settings;
+
+    @Autowired
+    public JwtTokenFactory(JwtSettings settings) {
+        this.settings = settings;
+    }
+
+    /**
+     * Factory method for issuing new JWT Tokens.
+     *
+     * @param username
+     * @param roles
+     * @return
+     */
+    public AccessJwtToken createAccessJwtToken(UserContext userContext) {
+        if (StringUtils.isBlank(userContext.getUsername()))
+            throw new IllegalArgumentException("Cannot create JWT Token without username");
+
+        if (userContext.getAuthorities() == null || userContext.getAuthorities().isEmpty())
+            throw new IllegalArgumentException("User doesn't have any privileges");
+
+        Claims claims = Jwts.claims().setSubject(userContext.getUsername());
+        claims.put("scopes", userContext.getAuthorities().stream().map(s -> s.toString()).collect(Collectors.toList()));
+
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuer(settings.getTokenIssuer())
+                .setIssuedAt(Date.from(currentTime.atZone(ZoneId.systemDefault()).toInstant()))
+                .setExpiration(Date.from(currentTime
+                        .plusMinutes(settings.getTokenExpirationTime())
+                        .atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(SignatureAlgorithm.HS512, settings.getTokenSigningKey())
+                .compact();
+
+        return new AccessJwtToken(token, claims);
+    }
+
+    public JwtToken createRefreshToken(UserContext userContext) {
+        if (StringUtils.isBlank(userContext.getUsername())) {
+            throw new IllegalArgumentException("Cannot create JWT Token without username");
+        }
+
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        Claims claims = Jwts.claims().setSubject(userContext.getUsername());
+        claims.put("scopes", Arrays.asList(Scopes.REFRESH_TOKEN.authority()));
+
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuer(settings.getTokenIssuer())
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(Date.from(currentTime.atZone(ZoneId.systemDefault()).toInstant()))
+                .setExpiration(Date.from(currentTime
+                        .plusMinutes(settings.getRefreshTokenExpTime())
+                        .atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(SignatureAlgorithm.HS512, settings.getTokenSigningKey())
+                .compact();
+
+        return new AccessJwtToken(token, claims);
+    }
+}
