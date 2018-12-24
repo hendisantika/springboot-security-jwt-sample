f4850fcc215f76ce2f14ce23dbfbed52d0081131
Hendi Santika <hendisantika@gmail.com>
2018-12-25 05:40:16 +0700

    #24 - Add Auth Method Not Supported Exception

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/exceptions/AuthMethodNotSupportedException.java
@@ -0,0 +1,21 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.exceptions;
+
+import org.springframework.security.authentication.AuthenticationServiceException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 05:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class AuthMethodNotSupportedException extends AuthenticationServiceException {
+    private static final long serialVersionUID = 3705043083010304496L;
+
+    public AuthMethodNotSupportedException(String msg) {
+        super(msg);
+    }
+}
