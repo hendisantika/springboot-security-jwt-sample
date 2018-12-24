3227af1bd686cd962bc8a8eee205f18a0e619aae
Hendi Santika <hendisantika@gmail.com>
2018-12-25 05:32:59 +0700

    #16 - Add User Context

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/model/UserContext.java
@@ -0,0 +1,39 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.model;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 05:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserContext {
+    private final String username;
+    private final List<GrantedAuthority> authorities;
+
+    private UserContext(String username, List<GrantedAuthority> authorities) {
+        this.username = username;
+        this.authorities = authorities;
+    }
+
+    public static UserContext create(String username, List<GrantedAuthority> authorities) {
+        if (StringUtils.isBlank(username)) throw new IllegalArgumentException("Username is blank: " + username);
+        return new UserContext(username, authorities);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/model/token/JwtToken.java
@@ -0,0 +1,14 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.model.token;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 05:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class JwtToken {
+}
