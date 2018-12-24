044b344e170e7b838428fcc4420e511f3352286c
Hendi Santika <hendisantika@gmail.com>
2018-12-25 06:31:40 +0700

    #31 - Add Login Request

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/auth/ajax/LoginRequest.java
@@ -0,0 +1,33 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.auth.ajax;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-25
+ * Time: 06:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    @JsonCreator
+    public LoginRequest(@JsonProperty("username") String username, @JsonProperty("password") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
