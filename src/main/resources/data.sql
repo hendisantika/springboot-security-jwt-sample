bed378391db6a6dd0b5566e8cef5be03f9ef294a
Hendi Santika <hendisantika@gmail.com>
2018-12-26 06:04:27 +0700

    #45 - Add more details in README.md file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into APP_USER(ID, PASSWORD, USERNAME) values(1, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', 'hendisantika@gmail.com');
+insert into APP_USER(ID, PASSWORD, USERNAME) values(1, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', 'hendisantika@gmail.com'); -- test1234
 insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'ADMIN');
 insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'PREMIUM_MEMBER');
