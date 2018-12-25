5407d87b69d3ce08db14129f412daf70a13cc85c
Hendi Santika <hendisantika@gmail.com>
2018-12-26 05:42:36 +0700

    #40 - Add Refresh Token

---
+++ src/main/java/com/hendisantika/jwt/springbootsecurityjwtsample/security/model/token/RefreshToken.java
@@ -0,0 +1,65 @@
+package com.hendisantika.jwt.springbootsecurityjwtsample.security.model.token;
+
+import com.hendisantika.jwt.springbootsecurityjwtsample.security.model.Scopes;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-security-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-26
+ * Time: 05:42
+ * To change this template use File | Settings | File Templates.
+ */
+@SuppressWarnings("unchecked")
+public class RefreshToken implements JwtToken {
+    private Jws<Claims> claims;
+
+    private RefreshToken(Jws<Claims> claims) {
+        this.claims = claims;
+    }
+
+    /**
+     * Creates and validates Refresh token
+     *
+     * @param token
+     * @param signingKey
+     * @return
+     * @throws BadCredentialsException
+     * @throws JwtExpiredTokenException
+     */
+    public static Optional<RefreshToken> create(RawAccessJwtToken token, String signingKey) {
+        Jws<Claims> claims = token.parseClaims(signingKey);
+
+        List<String> scopes = claims.getBody().get("scopes", List.class);
+        if (scopes == null || scopes.isEmpty()
+                || !scopes.stream().filter(scope -> Scopes.REFRESH_TOKEN.authority().equals(scope)).findFirst().isPresent()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new RefreshToken(claims));
+    }
+
+    @Override
+    public String getToken() {
+        return null;
+    }
+
+    public Jws<Claims> getClaims() {
+        return claims;
+    }
+
+    public String getJti() {
+        return claims.getBody().getId();
+    }
+
+    public String getSubject() {
+        return claims.getBody().getSubject();
+    }
+}
